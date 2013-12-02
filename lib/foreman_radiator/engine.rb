cf83ed0f474159005ad76ab73c9d92b5202ff716
Amos Benari <abenari@redhat.com>
2013-12-02 11:45:55 +0200

    register plugin

--- lib/foreman_radiator/engine.rb
+++ lib/foreman_radiator/engine.rb
@@ -2,6 +2,9 @@ require 'foreman_radiator'
 
 module ForemanRadiator
   class Engine < ::Rails::Engine
-
+    initializer 'foreman_radiator.register_plugin', :after=> :finisher_hook do |app|
+      Foreman::Plugin.register :foreman_radiator do
+      end if defined? Foreman::Plugin
+    end
   end
 end
